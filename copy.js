cc9768362dedb4b820164f1fc47e704293818dde
Mates24 <matejcikmathias@gmail.com>
2024-04-17 10:05:41 +0200

    Script for password copying

---
+++ copy.js
@@ -0,0 +1,9 @@
+function copy(){
+    var copyText = document.getElementById('password');
+
+    copyText.select();
+    copyText.setSelectionRange(0, 99999);
+
+    navigator.clipboard.writeText(copyText.value);
+    alert("Copied");
+}
